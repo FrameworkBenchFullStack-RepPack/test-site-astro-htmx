36b833fcee8cb2001e43ba6788e4e49df64a1c74
Anton <dev@atjn.dk>
2026-03-20 14:44:15 +0100

    Fix bad CSP

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,17 +10,6 @@ export default defineConfig({
     mode: "middleware",
     staticHeaders: true,
   }),
-  security: {
-    csp: {
-      directives: [
-        "default-src 'self' data:",
-        "base-uri 'none'",
-        "frame-ancestors 'none'",
-        "object-src 'none'",
-        "form-action 'self'",
-      ],
-    },
-  },
   env: {
     schema: {
       DATABASE_URL: envField.string({ context: "server", access: "secret" }),
--- src/assets/headers.ts
+++ src/assets/headers.ts
@@ -3,4 +3,8 @@ export const securityHeaders = [
   ["Cross-Origin-Embedder-Policy", "require-corp"],
   ["Cross-Origin-Opener-Policy", "same-origin"],
   ["X-Content-Type-Options", "nosniff"],
+  [
+    "Content-Security-Policy",
+    "default-src 'self' 'unsafe-inline' data: ; form-action 'self'; base-uri 'none'; frame-ancestors 'none'; object-src 'none';",
+  ],
 ];
